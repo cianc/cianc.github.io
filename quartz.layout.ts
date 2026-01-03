3be31de3e40602ccf4c33944449a6b6f42cf0594
Cian Cullinan <cian.cullinan@gmail.com>
2026-01-03 12:18:36 +0000

    Quartz sync: Jan 3, 2026, 12:18 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,12 +39,12 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-//    Component.DesktopOnly(
+    Component.DesktopOnly(
 	Component.RecentNotes({
 		showTags: false,
 		limit: 4,
 		linkToMore: "posts/" as SimpleSlug,
-    		}),//),    
+    		})),    
   ],
   right: [
     //Component.Graph(),
